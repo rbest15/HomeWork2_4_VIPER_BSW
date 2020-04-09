7b50acc8074a629f0a6b3bec77266b2d09920c31
moremist <rbest15@rambler.ru>
2020-04-09 16:12:47 +0300

    Initial Commit

---
+++ HomeWork2_4_VIPER_BSW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork2_4_VIPER_BSW
+//
+//  Created by Roman Kuzmich on 09.04.2020.
+//  Copyright © 2020 Roman Kuzmich Home Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
